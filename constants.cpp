d4e450df425f49580ca64b069b43f4d9a7cc77dc
MarkVerbeek91 <mark.verbeek91@gmail.com>
2014-11-19 10:42:38 +0100

    started to work towards a solution. The variable are defined.

--- constants.cpp
+++ constants.cpp
@@ -1,3 +1,6 @@
+
+#include "constants.h"
+
 /**
  * Declaration of constants and tables. Here the properties of a fusor are
  * descripted. Some phicical constants defined like charge of a proton and
@@ -7,10 +10,7 @@
  *
  */
 
-#ifndef CONSTANTS_H_INCLUDED
-#define CONSTANTS_H_INCLUDED
-
-extern struct Fusor{
+struct Fusor{
     double a = 0.05;
     double b = 0.25;
     double V0 = -40000; // voltage
@@ -23,16 +23,16 @@ extern struct Fusor{
 #define N_PRECISION 250
 #define N_TABLE     250
 
-extern double q = 1.602e-19;
-extern double pressure = 0.5;  // Pa
-extern double Tgas = 400; // K
-extern double ngas = 9.05401e19; //6.022e23 * pressure / (8.314 * Tgas);
-extern double E0 = 0.0001;          // reducing errors
-extern double Itot = 0.1;
-extern double EdgeIonFlux;
+double q = 1.602e-19;
+double pressure = 0.5;  // Pa
+double Tgas = 400; // K
+double ngas = 9.05401e19; //6.022e23 * pressure / (8.314 * Tgas);
+double E0 = 0.0001;          // reducing errors
+double Itot = 0.1;
+double EdgeIonFlux;
 
 // some data storage is now needed because other wise the calculation becomes hugh
-extern struct Tables{
+struct Tables{
     double R[N_TABLE];
     double A[N_TABLE];
     double K[N_TABLE][N_TABLE];
@@ -44,4 +44,3 @@ extern struct Tables{
     double S_5[N_TABLE];
 } Table;
 
-#endif // CONSTANTS_H_INCLUDED
