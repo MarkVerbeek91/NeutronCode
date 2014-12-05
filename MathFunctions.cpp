eb46b7cc20ee2ca6e320965697577dca34d757eb
MarkVerbeek91 <mark.verbeek91@gmail.com>
2014-12-05 18:12:09 +0100

    started working on the upgrade of the other two integration functions that are implementend. The upgrade is from ... methode to the Adaptive Simpsons rule.
    However I came stuck on the facked that probably integrate function 2 and 3 are wrongly used or that the comments are wrong.

--- MathFunctions.cpp
+++ MathFunctions.cpp
@@ -34,43 +34,63 @@ double interpolation(double r)
     return value;
 }
 
+/** This section of code uses the Adaptive Simpsons rule to calculate the
+    integral. It uses a recursive way to minimize to error.
+
+*/
 double adaptiveSimpsonsAux(double (*f)(double), double a, double b, double epsilon,
-                         double S, double fa, double fb, double fc, int bottom) {
-  double c = (a + b)/2, h = b - a;
-  double d = (a + c)/2, e = (c + b)/2;
-  double fd = f(d), fe = f(e);
-  double Sleft = (h/12)*(fa + 4*fd + fc);
-  double Sright = (h/12)*(fc + 4*fe + fb);
-  double S2 = Sleft + Sright;
-  if (bottom <= 0 || fabs(S2 - S) <= 15*epsilon)
-    return S2 + (S2 - S)/15;
-  return adaptiveSimpsonsAux(f, a, c, epsilon/2, Sleft,  fa, fc, fd, bottom-1) +
-         adaptiveSimpsonsAux(f, c, b, epsilon/2, Sright, fc, fb, fe, bottom-1);
+                           double S, double fa, double fb, double fc, int bottom)
+{
+    double c = (a + b)/2, h = b - a;
+    double d = (a + c)/2, e = (c + b)/2;
+    double fd = f(d), fe = f(e);
+    double Sleft = (h/12)*(fa + 4*fd + fc);
+    double Sright = (h/12)*(fc + 4*fe + fb);
+    double S2 = Sleft + Sright;
+    if (bottom <= 0 || fabs(S2 - S) <= 15*epsilon)
+        return S2 + (S2 - S)/15;
+
+    return adaptiveSimpsonsAux(f, a, c, epsilon/2, Sleft,  fa, fc, fd, bottom-1) +
+           adaptiveSimpsonsAux(f, c, b, epsilon/2, Sright, fc, fb, fe, bottom-1);
 }
 
-double NIntegration( double (*f)(double), double a, double b)
+double NIntegration( double (*funcPtr)(double), double Start, double End)
 {
     double epsilon = 1.0 / PRECISION;
 
-    double c = (a + b)/2, h = b - a;
-    double fa = f(a), fb = f(b), fc = f(c);
-    double S = (h/6)*(fa + 4*fc + fb);
+    double Mid = (Start + End)/2, h = End - Start;
+    double funcStart = funcPtr(Start), funcEnd= funcPtr(End), funcMid = funcPtr(Mid);
+    double S = (h/6)*(funcStart + 4*funcMid + funcEnd);
 
-    return adaptiveSimpsonsAux(f, a, b, epsilon, S, fa, fb, fc, MAX_RECURSION_DEPTH);
+    return adaptiveSimpsonsAux(funcPtr, Start, End, epsilon, S, funcStart, funcEnd, funcMid, MAX_RECURSION_DEPTH);
 }
 
-double NIntegration_2( double (*funcPtr)(double, double), double Bar, double Start, double End)
+double adaptiveSimpsonsAux2(double (*f)(double, double), double a, double b, double bar, double epsilon,
+                           double S, double fa, double fb, double fc, int bottom)
 {
-    double sum = (*funcPtr)(Bar,Start) + (*funcPtr)(Bar,End), step = (End - Start)/N_PRECISION;
+    double c = (a + b)/2, h = b - a;
+    double d = (a + c)/2, e = (c + b)/2;
+    double fd = f(bar, d), fe = f(bar, e);
+    double Sleft = (h/12)*(fa + 4*fd + fc);
+    double Sright = (h/12)*(fc + 4*fe + fb);
+    double S2 = Sleft + Sright;
+    if (bottom <= 0 || fabs(S2 - S) <= 15*epsilon)
+        return S2 + (S2 - S)/15;
 
-    for (double r=Start; r<End; r += step)
-    {
-        sum += 2.0 * (*funcPtr)(Bar, r);
-    }
+    return adaptiveSimpsonsAux2(f, a, c, bar, epsilon/2, Sleft,  fa, fc, fd, bottom-1) +
+           adaptiveSimpsonsAux2(f, c, b, bar, epsilon/2, Sright, fc, fb, fe, bottom-1);
+}
 
-    sum = sum * step / 2.0;
 
-    return sum;
+double NIntegration_2( double (*funcPtr)(double, double), double bar, double Start, double End)
+{
+    double epsilon = 1.0 / PRECISION;
+
+    double Mid = (Start + End)/2, h = End - Start;
+    double funcStart = funcPtr(bar, Start), funcEnd= funcPtr(bar, End), funcMid = funcPtr(bar, Mid);
+    double S = (h/6)*(funcStart + 4*funcMid + funcEnd);
+
+    return adaptiveSimpsonsAux2(funcPtr, Start, End, bar, epsilon, S, funcStart, funcEnd, funcMid, MAX_RECURSION_DEPTH);
 }
 
 // special integrator for the Class II ions.
