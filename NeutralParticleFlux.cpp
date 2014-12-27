28935f1810d6281105e1e60b0a0c7ac2f230ee6a
MarkVerbeek91 <mark.verbeek91@gmail.com>
2014-12-27 20:47:18 +0100

    Add files for Neutral Particel Flux

---
+++ NeutralParticleFlux.cpp
@@ -0,0 +1,27 @@
+
+
+// neutral particle flux from I class ions, outside the cathode, inwards.
+
+
+// neutral particle flux from I class ions, outside the cathode, outwards.
+
+
+// neutral particle flux from I class ions, inside the cathode, inwards.
+
+
+// neutral particle flux from I class ions, inside the cathode, outwards.
+
+
+
+// neutral particle flux from II class ions, outside the cathode, inwards.
+
+
+// neutral particle flux from II class ions, outside the cathode, outwards.
+
+
+// neutral particle flux from II class ions, inside the cathode, inwards.
+
+
+// neutral particle flux from II class ions, inside the cathode, outwards.
+
+
