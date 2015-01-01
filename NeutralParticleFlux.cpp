bc9817da247d49d7bea745bedd42df4247f803b7
MarkVerbeek91 <mark.verbeek91@gmail.com>
2015-01-01 22:18:01 +0100

    First two particle flux functions implemented

--- NeutralParticleFlux.cpp
+++ NeutralParticleFlux.cpp
@@ -1,9 +1,53 @@
 
+#include "math.h"
+
+#include "constants.h"
+#include "CrossSections.h"
+#include "SurvivalFunctions.h"
+#include "PotentialFunctions.h"
+#include "MathFunctions.h"
 
 // neutral particle flux from I class ions, outside the cathode, inwards.
+double Sfn1_InMinInte(double r, double dr )
+{
+    double fac  = CrosssecFusion(ParticleEnergy2(r,dr)) * pow(giveAnodeRadius()/r,2) ;
+           fac *= ngas * EdgeIonFlux * f(dr);
 
+    return fac;
+}
+
+double Sfn1_InMin( double r )
+{
+    double S;
+
+    double (*Sfn1_InMinIntePtr)(double, double);
+    Sfn1_InMinIntePtr = &Sfn1_InMinInte;
+
+    S = NIntegration_2(*Sfn1_InMinIntePtr, r, r, giveAnodeRadius());
+
+    return S;
+}
 
 // neutral particle flux from I class ions, outside the cathode, outwards.
+double Sfn1_OutMinInte(double r, double dr )
+{
+    double fac  = CrosssecFusion(ParticleEnergy2(r,dr)) * pow(giveAnodeRadius()/r,2) ;
+           fac *= ngas * EdgeIonFlux * f(dr);
+
+    return fac;
+}
+
+double Sfn1_OutMin( double r )
+{
+    double S;
+
+    double (*Sfn1_OutMinIntePtr)(double, double);
+    Sfn1_OutMinIntePtr = &Sfn1_OutMinInte;
+
+    S = NIntegration_2(*Sfn1_OutMinIntePtr, r, r, giveAnodeRadius());
+
+    return S;
+}
 
 
 // neutral particle flux from I class ions, inside the cathode, inwards.
---
+++ NeutralParticleFlux.h
@@ -0,0 +1,7 @@
+#ifndef NEUTRALPARTICLEFLUX_H_INCLUDED
+#define NEUTRALPARTICLEFLUX_H_INCLUDED
+
+double Sfn1_InMinInte(double r, double dr );
+double Sfn1_InMin( double r );
+
+#endif // NEUTRALPARTICLEFLUX_H_INCLUDED
