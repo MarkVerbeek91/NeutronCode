8e02dda4f051977376fa6b5e242b6817563b1ecd
MarkVerbeek91 <mark.verbeek91@gmail.com>
2015-01-02 11:39:35 +0100

    Neutron Production from fast Neutrals created by Class I ions added.
    Also updated the output to screen.

--- NeutronProductionRate.cpp
+++ NeutronProductionRate.cpp
@@ -8,13 +8,14 @@
 
 #include "constants.h"
 #include "MathFunctions.h"
-#include "ParticleFlux.h"
+#include "IonParticleFlux.h"
+#include "NeutralParticleFlux.h"
 #include "NeutronProductionRate.h"
 
 
 double Nps(void)
 {
-    double NPS;
+    double NPS, tempNPS;
 
     double (*Sfi_InMinPtr)(double);
     Sfi_InMinPtr = &Sfi_InMin;
@@ -27,12 +28,40 @@ double Nps(void)
 
     printf(" - Ions inwards inside cathode\n");
     NPS  = NIntegration(*Sfi_InMinPtr, 0.01, giveCathodeRadius() - 0.000001);
-    printf(" - Ions outwards inside cathode\n");
+    printf("\t %E \n - Ions outwards inside cathode\n");
     NPS += NIntegration(*Sfi_InPlusPtr, 0.01, giveCathodeRadius() - 0.000001);
-    printf(" - Ions inward outside cathode\n");
+    printf("\t %E \n - Ions inward outside cathode\n");
     NPS += NIntegration(*Sfi_OutMinPtr, giveCathodeRadius(), giveAnodeRadius() - 0.000001);
-    printf(" - Ions outwards outside cathode\n");
+    printf("\t %E \n - Ions outwards outside cathode\n");
     NPS += NIntegration(*Sfi_OutPlusPtr, giveCathodeRadius(), giveAnodeRadius() - 0.000001);
+    printf("\t %E \n", tempNPS);
+
+    double (*Sfn1_InMinPtr)(double);
+    Sfn1_InMinPtr = &Sfn1_InMin;
+
+    double (*Sfn1_OutMinPtr)(double);
+    Sfn1_OutMinPtr = &Sfn1_OutMin;
+
+    double Sfn1_InPlus(double r);
+    double (*Sfn1_InPlusPtr)(double);
+    Sfn1_InPlusPtr = &Sfn1_InPlus;
+
+    double (*Sfn1_OutPlusPtr)(double);
+    Sfn1_OutPlusPtr = &Sfn1_OutPlus;
+
+    printf(" - Neutrals inwards inside cathode from Class I\n");
+    tempNPS = NIntegration(*Sfn1_InMinPtr, 0.01, giveCathodeRadius() - 0.000001);
+    NPS  = tempNPS;
+    printf("\t %E \n - Neutrals outwards inside cathode from Class I\n", tempNPS);
+    tempNPS = NIntegration(*Sfn1_OutMinPtr, 0.01, giveCathodeRadius() - 0.000001);
+    NPS += tempNPS;
+    printf("\t %E \n - Neutrals inward outside cathode from Class I\n", tempNPS);
+    tempNPS = NIntegration(*Sfn1_InPlusPtr, giveCathodeRadius(), giveAnodeRadius() - 0.000001);
+    NPS += tempNPS;
+    printf("\t %E \n - Neutrals outwards outside cathode from Class I\n", tempNPS);
+    tempNPS = NIntegration(*Sfn1_OutPlusPtr, giveCathodeRadius(), giveAnodeRadius() - 0.000001);
+    NPS += tempNPS;
+    printf("\t %E \n", tempNPS);
 
     return NPS;
 }
