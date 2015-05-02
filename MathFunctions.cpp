e196d2962bd572eba2dcaa01ce41ff349ad9875a
MarkVerbeek91 <mark.verbeek91@gmail.com>
2015-05-02 21:18:05 +0200

    A thirt NIntegrator is added because the neutral flux from Class II ions outwards outside the cathode needed to integrate a function with 3 parameters instead of 2.

--- MathFunctions.cpp
+++ MathFunctions.cpp
@@ -34,6 +34,30 @@ double interpolation(double r)
 
     return value;
 }
+/*
+double Interpolation(double* Table[], double r)
+{
+    double value = -1;
+    int low = N_TABLE - 2, upper = N_TABLE - 1;
+
+    for ( int i = 0; i < N_TABLE; i++)
+    {
+        if ( r < Table.R[i])
+        {
+            low = i - 1;
+            upper = i;
+        }
+    }
+
+    double slope = ( Table.S[upper] - Table.S[low])/(Table.R[upper] - Table.R[low]);
+
+    value = slope*r + Table.S[low] - Table.R[low] * slope;
+
+    if (value < 0)
+        value *= -1;
+
+    return value;}
+*/
 
 /** This section of code uses the Adaptive Simpsons rule to calculate the
     integral. It uses a recursive way to minimize to error.
@@ -55,6 +79,7 @@ double adaptiveSimpsonsAux(double (*f)(double), double a, double b, double epsil
            adaptiveSimpsonsAux(f, c, b, epsilon/2, Sright, fc, fb, fe, bottom-1);
 }
 
+// an integration function for functions like: X(r) over r
 // TODO: make more rebust. no infinte loops posible and such.
 double NIntegration( double (*funcPtr)(double), double Start, double End)
 {
@@ -80,10 +105,10 @@ double adaptiveSimpsonsAux2(double (*f)(double, double), double a, double b, dou
         return S2 + (S2 - S)/15;
 
     return adaptiveSimpsonsAux2(f, a, c, epsilon/2, var, Sleft,  fa, fc, fd, bottom-1) +
-           adaptiveSimpsonsAux2(f, c, b, var, epsilon/2, Sright, fc, fb, fe, bottom-1);
+           adaptiveSimpsonsAux2(f, c, b, epsilon/2, var, Sright, fc, fb, fe, bottom-1);
 }
 
-// special integrator for the Class II ions.
+// An integration function for functions like: X(r, dr) over dr
 // TODO: make more robust
 double NIntegration_2( double (*funcPtr)(double, double), double var, double Start, double End)
 {
@@ -95,6 +120,36 @@ double NIntegration_2( double (*funcPtr)(double, double), double var, double Sta
     double S = (h/6)*(funcStart + 4*funcMid + funcEnd);
 
     return adaptiveSimpsonsAux2(funcPtr, Start, End, epsilon, var, S, funcStart, funcEnd, funcMid, MAX_RECURSION_DEPTH);
+ }
+
+double adaptiveSimpsonsAux3(double (*f)(double, double, double), double a, double b, double epsilon, double var1, double var2,
+                           double S, double fa, double fb, double fc, int bottom)
+{
+    double c = (a + b)/2, h = b - a;
+    double d = (a + c)/2, e = (c + b)/2;
+    double fd = f(var1, var2, d), fe = f(var1, var2, e);
+    double Sleft = (h/12)*(fa + 4*fd + fc);
+    double Sright = (h/12)*(fc + 4*fe + fb);
+    double S2 = Sleft + Sright;
+    if (bottom <= 0 || fabs(S2 - S) <= 15*epsilon)
+        return S2 + (S2 - S)/15;
+
+    return adaptiveSimpsonsAux3(f, a, c, epsilon/2, var1, var2, Sleft,  fa, fc, fd, bottom-1) +
+           adaptiveSimpsonsAux3(f, c, b, epsilon/2, var1, var2,  Sright, fc, fb, fe, bottom-1);
+}
+
+// An integration function for functions like: X(E, r, ddr) over ddr
+// TODO: make more robust
+double NIntegration_3( double (*funcPtr)(double, double, double), double var1, double var2, double Start, double End)
+{
+    double epsilon = 1.0 / PRECISION;
+
+    double Mid = (Start + End)/2, h = End - Start;
+
+    double funcStart = funcPtr(var1, var2, Start), funcEnd = funcPtr(var1, var2, End), funcMid = funcPtr(var1, var2, Mid);
+    double S = (h/6)*(funcStart + 4*funcMid + funcEnd);
+
+    return adaptiveSimpsonsAux3(funcPtr, Start, End, epsilon, var1, var2, S, funcStart, funcEnd, funcMid, MAX_RECURSION_DEPTH);
  }
 
 // this function gives of the slope of the function at point.
@@ -116,3 +171,18 @@ bool DELTA(double d)
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
