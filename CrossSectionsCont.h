5344c6fb1578f20191e0a0bd35e450f858996aa1
MarkVerbeek91 <mark.verbeek91@gmail.com>
2015-08-14 15:57:41 +0200

    Made that the constands for the cross sections can not be changed by making then ... constands.

--- CrossSectionsCont.h
+++ CrossSectionsCont.h
@@ -11,31 +11,31 @@
     Coefficients for the CX cross section of deuterium
 */
 struct CX_cross_sections{
-    double A1cx =   3.245     ;
-    double A2cx = 235.88      ;
-    double A3cx =   0.038371  ;
-    double A4cx =   3.8068e-6 ;
-    double A5cx =   1.1832e-10;
-    double A6cx =   2.3713    ;
+    const double A1cx =   3.245     ;
+    const double A2cx = 235.88      ;
+    const double A3cx =   0.038371  ;
+    const double A4cx =   3.8068e-6 ;
+    const double A5cx =   1.1832e-10;
+    const double A6cx =   2.3713    ;
 } CS_cx;
 
 struct Ion_impact_cross_sections{
-    double A1Ion = 12.899    ;
-    double A2Ion = 61.897    ;
-    double A3Ion =  9.27e3   ;
-    double A4Ion =  4.9749e-4;
-    double A5Ion =  3.989e-2 ;
-    double A6Ion = -1.59     ;
-    double A7Ion =  3.1834   ;
-    double A8Ion = -3.7154   ;
+    const double A1Ion = 12.899    ;
+    const double A2Ion = 61.897    ;
+    const double A3Ion =  9.27e3   ;
+    const double A4Ion =  4.9749e-4;
+    const double A5Ion =  3.989e-2 ;
+    const double A6Ion = -1.59     ;
+    const double A7Ion =  3.1834   ;
+    const double A8Ion = -3.7154   ;
 } CS_Ion;
 
 struct Fusion_cross_section{
-    double A1Fusion =  47.88     ;
-    double A2Fusion = 482        ;
-    double A3Fusion =   3.08e-4  ;
-    double A4Fusion =   1.177    ;
-    double A5Fusion =   0        ;
+    const double A1Fusion =  47.88     ;
+    const double A2Fusion = 482        ;
+    const double A3Fusion =   3.08e-4  ;
+    const double A4Fusion =   1.177    ;
+    const double A5Fusion =   0        ;
 } CS_Fusion;
 
 
