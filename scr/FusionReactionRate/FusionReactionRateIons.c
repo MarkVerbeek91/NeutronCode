1a3b80f94eb078c0969ffb015a3a5b128e81bf40
MarkVerbeek91 <mark.verbeek91@gmail.com>
2016-01-18 15:39:26 +0100

    implemented the functions although the are not working as expected. Cross secion get a negative energy. And Ion spectrum does not get the correct values for radii.

--- scr/FusionReactionRate/FusionReactionRateIons.c
+++ scr/FusionReactionRate/FusionReactionRateIons.c
@@ -1,3 +1,35 @@
 /**
-    Here comes the particles flux.
+    The function intergrate the particle energy spectra to give a fussion
+    rate on every radii. Intergrating these function should give a total
+    neutron flux.
 */
+
+#include "constants.h"
+#include "MathFunctions.h"
+#include "IonSpectrum.h"
+#include "CrossSections.h"
+
+#include "FusionReactionRateIons.h"
+
+double FusionRateIons_Inte(double r, double E)
+{
+    double FRR;
+
+    FRR  = IonSpectrumInwards(r, E);
+    FRR += IonSpectrumOutwards(r, E);
+    FRR *= CrosssecFusion(E);
+
+    return FRR;
+}
+
+double FusionRateIons(double r)
+{
+    double FRR;
+    double (*funcPtr)(double, double);
+
+    funcPtr = &FusionRateIons_Inte;
+    FRR  = NIntegration_2(funcPtr, r, 0, fusor->V0);
+
+    FRR *= ngas;
+    return FRR;
+}
